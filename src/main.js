adda624ef8a212ce64c14a694d25973d1932ec85
alanren515 <416148752@qq.com>
2020-09-13 01:41:33 +0900

    3rdedit

---
+++ src/main.js
@@ -0,0 +1,42 @@
+import Vue from "vue";
+import './style.scss';
+
+import genres from './util/genres';
+
+new Vue({
+  el: '#app',
+  components: {
+    'movie-list': {
+      template: `<div id="movie-list">
+                    <div v-for = "movie in movies" class="movie"> {{movie.title}} </div>
+                </div>`,
+      data () {
+        return {
+          movies: [
+            {title: 'Polp fiction'},
+            {title: 'titanic'},
+            {title: 'American Pie'},
+          ]
+      };
+      }
+    },
+    'movie-filter': {
+      data(){
+        return{
+          genres
+        }
+      },
+      template: `<div id="movie-filter">             
+                    <h2>Filter results</h2>
+                    <div class="filter-group">
+                    <check-filter v-for ="genre in genres"></check-filter>
+                    </div>
+                </div>`,
+      components: {
+        'check-filter': {
+          template: `<div>Filter </div>`
+        }
+      }
+    }
+  }
+});
